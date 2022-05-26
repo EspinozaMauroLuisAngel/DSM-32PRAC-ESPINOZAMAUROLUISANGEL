e0fc49ae54d2a16e7568696891abda58a3b7e2a3
EspinozaMauroLuisAngel <105399243+EspinozaMauroLuisAngel@users.noreply.github.com>
2022-05-25 22:58:10 -0500

    Add files via upload

---
+++ funcionesdeclarativas.js
@@ -0,0 +1,36 @@
+
+// Ejemplo de funciones declarativas
+
+function suma (){
+    var num1=10; 
+    var num2=90;
+    return num1+num2;
+}
+suma();
+
+function resta (){
+    var num1=100; 
+    var num2=50;
+    return num1-num2;
+}
+resta();
+
+function multiplicacion(){
+    var num1=10; 
+    var num2=10;
+    return num1*num2;
+}
+multiplicacion();
+
+function division (){
+    var num1=8; 
+    var num2=4;
+    return num1/num2;
+}
+division();
+
+function areaCuadrado (){
+    var lado=10; 
+    return lado*lado;
+}
+areaCuadrado();
