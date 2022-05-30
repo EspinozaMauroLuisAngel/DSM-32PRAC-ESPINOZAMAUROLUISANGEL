778a337f6b1b06b8e3bdb6e95ad23c3c2a058b55
EspinozaMauroLuisAngel <105399243+EspinozaMauroLuisAngel@users.noreply.github.com>
2022-05-30 17:35:23 -0500

    Add files via upload

---
+++ espinoza/condicionales.js
@@ -0,0 +1,52 @@
+// Ejemplo de Cuevita
+var Juanito = 18;
+if  (Juanito == 18){
+    console.log ("Juanito si puede entrar a la cuevita");
+}else{
+    console.log ("Juanito no puede entrar a la cuevita");
+}
+var Juanito = 17;
+if  (Juanito == 18){
+    console.log ("Juanito si puede entrar a la cuevita");
+}else{
+    console.log ("Juanito no puede entrar a la cuevita");
+}
+// Ejemplo de BadBunny
+var personas = 250;
+if (personas == 250){
+    console.log ("Se Agotaron los boletos para el Concierto de BadBunny")
+}else{
+    console.log ("Si hay boletos para el Concierto de BadBunny")
+}
+var personas = 200;
+if (personas == 250){
+    console.log ("Se Agotaron los boletos para el Concierto de BadBunny")
+}else{
+    console.log ("Si hay boletos para el Concierto de BadBunny")
+}
+// Ejemplo de Cerveza
+var cerveza = 18;
+if (cerveza == 18){
+    console.log ("Si Puedes Comprar Cerveza")
+}else{
+    console.log ("No Puedes Comprar Cerveza")
+}
+var cerveza = 17;
+if (cerveza == 18){
+    console.log ("Si Puedes Comprar Cerveza")
+}else{
+    console.log ("No Puedes Comprar Cerveza")
+}
+// Ejemplo de Calificacion 
+var calificacion = 8.0;
+if (calificacion == 8.0){
+    console.log ("Si Aprobaste la Materia")
+}else{
+    console.log ("No Aprobaste la Materia")
+}
+var calificacion = 7.0;
+if (calificacion == 8.0){
+    console.log ("Si Aprobaste la Materia")
+}else{
+    console.log ("No Aprobaste la Materia")
+}
