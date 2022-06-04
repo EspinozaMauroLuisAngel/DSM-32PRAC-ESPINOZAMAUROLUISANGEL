c9bfdf0b7edc4c5044560af66ccefb38ea79842b
EspinozaMauroLuisAngel <105399243+EspinozaMauroLuisAngel@users.noreply.github.com>
2022-06-04 18:25:49 -0500

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,34 @@
+// Ejemplo 1
+var años = 10;
+var resultado;
+switch (años){
+    case 10:
+        resultado = "Si Tienes 10 años";
+        break
+    case 20:
+        resultado = "Si Tienes 20 años";
+        break
+    case 30:
+        resultado = "Si Tienes 30 años";
+        break
+        default:
+            resultado = "No existe";
+            console.log ("resultado")
+}
+// Ejemplo 2
+var comida = "Hamburguesa";
+var resultado;
+switch (comida){
+    case"Hamburguesa":
+        resultado = "Ordenaste una Hamburguesa";
+        break
+    case"Papas":
+        resultado = "Ordenaste unas Papas";
+        break
+    case"Pizza":
+        resultado = "Ordenaste una Pizza";
+        break
+        default:
+            resultado = "No existe";
+            console.log ("resultado")
+}
