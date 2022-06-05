582c15df999485ac6f6329efb1b9a1b97066b05e
EspinozaMauroLuisAngel <105399243+EspinozaMauroLuisAngel@users.noreply.github.com>
2022-06-04 23:31:39 -0500

    Add files via upload

---
+++ for.js
@@ -0,0 +1,24 @@
+//1
+for (i = 0; i <= 9; i++ ){
+    console.log ("Luis Angel"); 
+    document.write("<br>")  
+} 
+//2
+for (i = 0; i <= 9; i++ ){
+    console.log ("Hola");   
+    document.write("<br>")
+}  
+//3
+var comida = ('papas','hamburguesa','tacos','pizza');
+for (var i = 0; i<= comida.length; i++ ){
+    console.log ("Comiste:",comida);
+}
+//4
+var tenis = ('nike','puma','jordan','lacoste');
+for (var i = 0; i<= tenis.length; i++ ){
+    console.log ("Tus tenis son:",tenis);
+}
+
+
+
+
